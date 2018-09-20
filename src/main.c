3af99277f886db5f226ccd178eaad606bc9cad8a
Tomoki Imai <tomo832@gmail.com>
2018-09-20 23:12:48 +0900

    Add my_main and startup.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
+    printf(
+        "\t.text\n"
+        "\t.globl my_main\n"
+        "my_main:\n"
+        "\tpushq %%rbp\n"
+        "\tmovq %%rsp, %%rbp\n"
+        "\tmovl $0, %%eax\n"
+        "\tpopq %%rbp\n"
+        "\tret\n");
     return 0;
 }
